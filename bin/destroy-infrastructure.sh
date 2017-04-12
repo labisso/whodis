d104ac27f78ecac0d491c8bb432fe498cd7a3403
David LaBissoniere <david@labisso.com>
2017-04-11 21:46:45 -0500

    Adjust destroy script

--- bin/destroy-infrastructure.sh
+++ bin/destroy-infrastructure.sh
@@ -5,6 +5,5 @@ WHODIS_ROOT="$(dirname "$(dirname "$(readlink "$0")")")"
 source $WHODIS_ROOT/bin/.common.sh
 
 echoBanner "Destroying CloudFormation stack"
-aws cloudformation delete-stack --stack-name whodis --output table \
-    --retain-resources "whodisDockerRepository" "whodisDeployBucket"
+aws cloudformation delete-stack --stack-name whodis --output table
 
