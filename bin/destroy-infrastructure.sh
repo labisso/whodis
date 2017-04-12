5e49955bf2d7939a9a38fd087b84ad96c5b0c969
David LaBissoniere <david@labisso.com>
2017-04-11 22:48:20 -0500

    Fix destroy script

--- bin/destroy-infrastructure.sh
+++ bin/destroy-infrastructure.sh
@@ -4,6 +4,12 @@ WHODIS_ROOT="$(dirname "$(dirname "$(readlink "$0")")")"
 
 source $WHODIS_ROOT/bin/.common.sh
 
+
+echoBanner "Emptying bucket and repository"
+DEPLOY_BUCKET=$(getStackResourceId whodisDeployBucket)
+aws s3 rm s3://${DEPLOY_BUCKET} --recursive
+aws ecr delete-repository --force --repository-name whodis
+
 echoBanner "Destroying CloudFormation stack"
 aws cloudformation delete-stack --stack-name whodis --output table
 
